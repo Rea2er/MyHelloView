2abdf20d9bf9ed41f1a28dc0343a04f4be5b3164
Ruize Nie <ruize.nie@gmail.com>
2024-04-29 22:12:50 -0300

    init set up

---
+++ Sources/MyHelloView/MyHelloView.swift
@@ -0,0 +1,23 @@
+//
+//  MyHelloView.swift
+//
+//
+//  Created by ruize on 2024-04-29.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+public struct MyHelloView: View {
+    public var body: some View {
+        Text("Hello, 慕课!")
+    }
+    public init() {
+        
+    }
+}
+
+@available(iOS 13.0, *)
+#Preview {
+    MyHelloView()
+}
